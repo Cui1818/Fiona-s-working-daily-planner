37a933655e683555893db32d18685f3fe5bcaddd
Fiona Li <fionali1818@gmail.com>
2023-04-05 16:25:45 +0100

    Adding assets/script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
 //display currentDay 
 var currentDay =$("#currentDay");
 currentDay.text(moment()).format("LLLL");
+
+//create array for buttons
+
 //local storge 
+
 //savebtn addeventlistener
+$(".saveBtn").on("click",function){
+    
+}
+
+
